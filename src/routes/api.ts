18ab3a559f360234e514739907bb146df96dd660
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-10 14:15:54 -0300

    Adicionando novas frase bd

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,9 +5,9 @@ import * as ApiControler from '../controllers/apiController';
 const router = Router();
 
 router.get("/ping", ApiControler.ping);
-
 router.get('/random', ApiControler.random)
-
 router.get('/name/:name', ApiControler.name);
+
+router.post("/frases", ApiControler.createPhrase);
 
 export default router;
